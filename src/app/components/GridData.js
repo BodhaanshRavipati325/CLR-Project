fc1bade3ebe3d3c76889c0cb09f8baad40006784
Bodhaansh Ravipati <br183@duke.edu>
2024-05-18 10:11:27 -0500

    Remove useless json

--- src/app/components/GridData.js
+++ src/app/components/GridData.js
@@ -37,52 +37,6 @@ export default function GridData(props) {
     >
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {list}
-        {/* <Grid xs={4}>
-          <Item>
-            <h2 id="dataKey">Liquidation Volume</h2>
-            <ItemData props={volume}></ItemData>
-          </Item>
-        </Grid>
-        <Grid xs={4}>
-          <Item>
-            <h2 id="dataKey">Long Investment Volume</h2>
-
-            <ItemData props={longValue} name="longValue"></ItemData>
-          </Item>
-        </Grid>
-        <Grid xs={4}>
-          <Item>
-            <h2 id="dataKey">Short Investment Volume</h2>
-
-            <ItemData props={shortValue} name="shortValue"></ItemData>
-          </Item>
-        </Grid>
-        <Grid xs={4}>
-          <Item>
-            <h2 id="dataKey">Rate</h2>
-
-            <ItemData props={rate}></ItemData>
-          </Item>
-        </Grid>
-        <Grid xs={4}>
-          <Item>
-            <h2 id="dataKey">Average Price</h2>
-
-            <ItemData props={averagePrice} name="averagePrice"></ItemData>
-          </Item>
-        </Grid>
-        <Grid xs={4}>
-          <Item>
-            <h2 id="dataKey">3e3e3</h2>
-
-          </Item>
-        </Grid>
-        <Grid xs={4}>
-          <Item>
-            <h2 id="dataKey">3e3e3</h2>
-
-          </Item>
-        </Grid> */}
       </Grid>
     </Box>
     </>
